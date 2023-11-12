3f81fa545810ce545f66d45765ed01b598d6df9c
royaly <81520338+royalygames@users.noreply.github.com>
2023-11-12 13:21:23 +0100

    update : add remove recommendation bar in the video page

--- content.js
+++ content.js
@@ -10,8 +10,24 @@ const observeAndRemoveElements = () => {
     for (let mutation of mutationsList) {
       for (let node of mutation.addedNodes) {
         if (node instanceof HTMLElement) {
-          if (node.matches("#shorts-container, ytd-rich-grid-renderer")) {
-            node.remove()
+          // Utilisez directement l'ID pour obtenir l'élément
+          const secondaryElement = document.getElementById("secondary");
+          const primaryElement = document.querySelector("ytd-rich-grid-renderer");
+          const shortElement = document.getElementById("shorts-container");
+          if (secondaryElement) {
+            secondaryElement.remove();
+            const video = document.querySelector('video')
+            const videoc = document.querySelector('ytd-watch-flexy')
+            if (video) {
+              video.style.borderRadius = '10px'
+              videoc.style.overflowY = 'hidden'
+            }
+          }
+          if (primaryElement) {
+            primaryElement.remove();
+          }
+          if (shortElement) {
+            shortElement.remove();
           }
         }
       }
@@ -21,22 +37,24 @@ const observeAndRemoveElements = () => {
   observer.observe(document.body, { childList: true, subtree: true });
 };
 
-window.addEventListener("load", () => {
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
   chrome.storage.sync.get(['removeYtdBrowse', 'msg'], (result) => {
     if (result.removeYtdBrowse === true) {
-      removeElement("#shorts-container");
-      removeElement("ytd-rich-grid-renderer");
-      const y = document.querySelector('ytd-two-column-browse-results-renderer')
-      const t = document.createElement('h1')
+      observeAndRemoveElements()
+      const t = document.createElement('h1');
       t.style.textAlign = "center";
       t.style.width = "100%";
       t.style.color = "#fff";
       t.style.fontSize = '40px';
-      t.innerHTML = `${result.msg}`
-      y.append(t)
-      observeAndRemoveElements();
+      t.innerHTML = `${result.msg}`;
+      const y = document.querySelector('ytd-two-column-browse-results-renderer');
+      console.log(y)
+      y.append(t);
     } else if (result.removeYtdBrowse === false) {
-        
+
     }
-});
+  });
 });
