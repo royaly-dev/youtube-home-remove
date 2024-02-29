a1586cb8955be41a5c65ed01b90b025365aad778
royaly <81520338+royaly-dev@users.noreply.github.com>
2024-02-29 03:56:29 +0100

    update

--- content.js
+++ content.js
@@ -10,24 +10,8 @@ const observeAndRemoveElements = () => {
     for (let mutation of mutationsList) {
       for (let node of mutation.addedNodes) {
         if (node instanceof HTMLElement) {
-          // Utilisez directement l'ID pour obtenir l'élément
-          const secondaryElement = document.getElementById("secondary");
-          const primaryElement = document.querySelector("ytd-rich-grid-renderer");
-          const shortElement = document.getElementById("shorts-container");
-          if (secondaryElement) {
-            secondaryElement.remove();
-            const video = document.querySelector('video')
-            const videoc = document.querySelector('ytd-watch-flexy')
-            if (video) {
-              video.style.borderRadius = '10px'
-              videoc.style.overflowY = 'hidden'
-            }
-          }
-          if (primaryElement) {
-            primaryElement.remove();
-          }
-          if (shortElement) {
-            shortElement.remove();
+          if (node.matches("#shorts-container, ytd-rich-grid-renderer")) {
+            node.remove()
           }
         }
       }
@@ -37,24 +21,22 @@ const observeAndRemoveElements = () => {
   observer.observe(document.body, { childList: true, subtree: true });
 };
 
-
-
-
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   chrome.storage.sync.get(['removeYtdBrowse', 'msg'], (result) => {
     if (result.removeYtdBrowse === true) {
-      observeAndRemoveElements()
-      const t = document.createElement('h1');
+      removeElement("#shorts-container");
+      removeElement("ytd-rich-grid-renderer");
+      const y = document.querySelector('ytd-two-column-browse-results-renderer')
+      const t = document.createElement('h1')
       t.style.textAlign = "center";
       t.style.width = "100%";
       t.style.color = "#fff";
       t.style.fontSize = '40px';
-      t.innerHTML = `${result.msg}`;
-      const y = document.querySelector('ytd-two-column-browse-results-renderer');
-      console.log(y)
-      y.append(t);
+      t.innerHTML = `${result.msg}`
+      y.append(t)
+      observeAndRemoveElements();
     } else if (result.removeYtdBrowse === false) {
-
+        
     }
-  });
+});
 });
